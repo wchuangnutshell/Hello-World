275cbceb75f6da53195c864dea85813c5418069c
wchuangnutshell <williamchuang@acm.org>
2015-04-14 23:18:28 -0700

    Create ex_2_12.py

---
+++ ex_2_12.py
@@ -0,0 +1,7 @@
+## question 12 solution ##
+
+deg_f = int(input("What is the temperature in Farenheit? "))
+
+deg_c = ((deg_f)-32) * 5 / 9 
+
+print(deg_c, " degrees Celsius is", deg_f, " degrees Farenheit.")
